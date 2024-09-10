28c0ea30a9fcf00dbf325c4b618cf975bf65f593
Shotaro Tsuda <shotarotsuda@Shotaros-MacBook-Pro.local>
2024-09-10 15:19:58 +0900

    instaced ufos, elminate boxes, and disabled shadows

--- src/script.js
+++ src/script.js
@@ -16,7 +16,10 @@ import {
   ACESFilmicToneMapping,
   Raycaster,
   Box3,
-  Box3Helper
+  Box3Helper,
+  InstancedMesh,
+  Object3D,
+  Matrix4
 } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
@@ -44,6 +47,7 @@ raycaster.far = 50;
 
 // Bounding Box (Hit Box)
 let ufosData = [];
+let ufoInstancedMesh;
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -53,32 +57,25 @@ controls.enableDamping = true;
 
 // Load, Model, Animate
 (async function () {
-  // let envMap
-  try{
-    
-    //HDRI
+  try {
+    // HDRI
     const hdrLoader = new HDRJPGLoader(renderer);
-    let envmapTexture = await hdrLoader.loadAsync('./cannon_1k.jpg')
-
-    scene.environment = envmapTexture.renderTarget.texture
-    scene.environment.mapping = EquirectangularReflectionMapping
+    let envmapTexture = await hdrLoader.loadAsync('./cannon_1k.jpg');
+    scene.environment = envmapTexture.renderTarget.texture;
+    scene.environment.mapping = EquirectangularReflectionMapping;
     scene.environmentIntensity = 1;
-    let envMap = envmapTexture.renderTarget.texture
-    envMap.mapping = EquirectangularReflectionMapping
 
     // Texture
     let textures = {
       bump: await new TextureLoader().loadAsync('./earthbump-min.jpg'),
-    }
+    };
 
     // Draco Loader
-    const dLoader = ( await new DRACOLoader().setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/').setDecoderConfig({type: 'js'}));
-    
+    const dLoader = new DRACOLoader().setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/').setDecoderConfig({ type: 'js' });
+
     // Continent
-    let continent = ( await new GLTFLoader().setDRACOLoader(dLoader).loadAsync('./continent-draco.glb')).scene.children[0];
-
+    let continent = (await new GLTFLoader().setDRACOLoader(dLoader).loadAsync('./continent-draco.glb')).scene.children[0];
     textures.bump.flipY = false;
-
     let continentMaterial = new MeshPhysicalMaterial({
       bumpMap: textures.bump,
       bumpScale: 10,
@@ -87,27 +84,21 @@ controls.enableDamping = true;
       transmission: 0.4,
       thickness: 0,
     });
-
     continent.traverse((o) => {
       if (o.isMesh) o.material = continentMaterial;
       o.castShadow = false;
-    })
-
-    const continentScale = 20
-    continent.scale.set(continentScale , continentScale, continentScale);
+    });
+    continent.scale.set(20, 20, 20);
     continent.rotation.y -= Math.PI * 1.389;
     continent.rotation.x -= Math.PI * 0.01;
-
-    continent.name = "continent"
-    
-    scene.add(continent)
+    continent.name = "continent";
+    scene.add(continent);
 
     // Ocean
     let ocean = new Mesh(
       new SphereGeometry(10, 70, 70),
       new MeshPhysicalMaterial({
         color: new Color("#006B6D"),
-        // envMap,
         envMapIntensity: 3,
         roughness: 0,
         transparent: true,
@@ -115,57 +106,99 @@ controls.enableDamping = true;
         opacity: 0.1
       }),
     );
-
     ocean.receiveShadow = true;
     ocean.name = "ocean";
-
     scene.add(ocean);
- 
 
-    // UFO
-    let ufo = ( await new GLTFLoader().setDRACOLoader(dLoader).loadAsync('./element-5-draco.glb')).scene.children[0];
+    // Load UFO model
+    let ufoGLTF = await new GLTFLoader().setDRACOLoader(dLoader).loadAsync('./flower_pedal_1_draco.glb');
+    console.log(ufoGLTF); // Log the structure of the loaded GLTF model
 
-    ufosData = [
-      // North => Plus South => Minus East => Plus West => Minus
-      makeUFO(ufo, scene, 3, 0.0405, -51.0561), // Brazil 1
-      makeUFO(ufo, scene, 2, -2.5307, -44.2989), // Brazil 2
-      makeUFO(ufo, scene, 3, 30.8406, -115.2838), // Baja California
-      makeUFO(ufo, scene, 2, 31.6904, -106.4245), // North America 1
-      makeUFO(ufo, scene, 3, 39.1031, -84.512), // North America 2
-      makeUFO(ufo, scene, 3, 64.961, -21.9408), // North America
-      makeUFO(ufo, scene, 3, 51.3755, -4.1427), // UK
-      makeUFO(ufo, scene, 3, 41.6488, -0.8891), // Spain 1,
-      makeUFO(ufo, scene, 3, 37.3891, -5.9845), // Spain 2
-      makeUFO(ufo, scene, 2, 41.3275, 19.8187 ), // Albania
-      makeUFO(ufo, scene, 3, 50.4504, 30.5245 ), // Ukraine
-      makeUFO(ufo, scene, 2, 41.0082, 28.9784 ), // Turkey1
-      makeUFO(ufo, scene, 1, 39.9334, 32.8597 ), // Turkey2
-      makeUFO(ufo, scene, 3, 31.0461, 34.8516 ), // Israel
-      makeUFO(ufo, scene, 3, 21.5292, 39.1611 ), // Saudi Arabia 1
-      makeUFO(ufo, scene, 2, 26.0667, 50.5577 ), // Bahrain
-      makeUFO(ufo, scene, 3, 14.4974, -14.4524 ), // Snegal
-      makeUFO(ufo, scene, 2, 9.082, 8.6753 ), // Nigeria
-      makeUFO(ufo, scene, 3, -30.5595, 22.9375 ), // South Africa
-      makeUFO(ufo, scene, 1, -22.3285, 24.6849), // Botswana
-      makeUFO(ufo, scene, 3, 28.6139 , 77.2088 ), // India 1
-      makeUFO(ufo, scene, 1, 21.1458, 79.0882 ), // India 2
-      makeUFO(ufo, scene, 3, 12.2529, 109.1899), // Vietnam
-      makeUFO(ufo, scene, 2, 24.8104, 113.5972 ), // Shaoguan China
-      makeUFO(ufo, scene, 3, 39.9042, 116.4074 ), // Haidian, Beijing, China
-    ]
+    // Traverse the scene to find the Mesh object
+    let ufoMesh;
+    ufoGLTF.scene.traverse((child) => {
+      if (child.isMesh) {
+        ufoMesh = child;
+      }
+    });
 
+    if (ufoMesh) {
+      let ufoGeometry = ufoMesh.geometry;
+      let ufoMaterial = new MeshPhysicalMaterial({
+        roughness: 1,
+        metalness: 1,
+        color: new Color("#dcfd7c"),
+        envMapIntensity: 1,
+      });
+
+      // Create InstancedMesh
+      const ufoCount = 200; // Number of UFOs
+      ufoInstancedMesh = new InstancedMesh(ufoGeometry, ufoMaterial, ufoCount);
+      scene.add(ufoInstancedMesh);
+
+      // Initialize UFOs
+      ufosData = [
+        // North => Plus South => Minus East => Plus West => Minus
+        { size: 3, lat: 0.0405, lng: -51.0561 }, // Brazil 1
+        { size: 2, lat: -2.5307, lng: -44.2989 }, // Brazil 2
+        { size: 3, lat: 30.8406, lng: -115.2838 }, // Baja California
+        { size: 2, lat: 31.6904, lng: -106.4245 }, // North America 1
+        { size: 3, lat: 39.1031, lng: -84.512 }, // North America 2
+        { size: 3, lat: 64.961, lng: -21.9408 }, // North America
+        { size: 3, lat: 51.3755, lng: -4.1427 }, // UK
+        { size: 3, lat: 41.6488, lng: -0.8891 }, // Spain 1
+        { size: 3, lat: 37.3891, lng: -5.9845 }, // Spain 2
+        { size: 2, lat: 41.3275, lng: 19.8187 }, // Albania
+        { size: 3, lat: 50.4504, lng: 30.5245 }, // Ukraine
+        { size: 2, lat: 41.0082, lng: 28.9784 }, // Turkey1
+        { size: 1, lat: 39.9334, lng: 32.8597 }, // Turkey2
+        { size: 3, lat: 31.0461, lng: 34.8516 }, // Israel
+        { size: 3, lat: 21.5292, lng: 39.1611 }, // Saudi Arabia 1
+        { size: 2, lat: 26.0667, lng: 50.5577 }, // Bahrain
+        { size: 3, lat: 14.4974, lng: -14.4524 }, // Senegal
+        { size: 2, lat: 9.082, lng: 8.6753 }, // Nigeria
+        { size: 3, lat: -30.5595, lng: 22.9375 }, // South Africa
+        { size: 1, lat: -22.3285, lng: 24.6849 }, // Botswana
+        { size: 3, lat: 28.6139, lng: 77.2088 }, // India 1
+        { size: 1, lat: 21.1458, lng: 79.0882 }, // India 2
+        { size: 3, lat: 12.2529, lng: 109.1899 }, // Vietnam
+        { size: 2, lat: 24.8104, lng: 113.5972 }, // Shaoguan China
+        { size: 3, lat: 39.9042, lng: 116.4074 }, // Haidian, Beijing, China
+
+        // Add 170 random locations
+        ...Array.from({ length: 170 }, () => ({
+          size: Math.floor(Math.random() * 3) + 1, // Random size between 1 and 3
+          lat: (Math.random() * 180 - 90).toFixed(4), // Random latitude between -90 and 90
+          lng: (Math.random() * 360 - 180).toFixed(4) // Random longitude between -180 and 180
+        }))
+      ]
+
+      ufosData.forEach((data, index) => {
+        const ufoObject = new Object3D();
+        data.rad = Math.random() * Math.PI * 0.45 + 0.5;
+        data.yOff = 10.3 + Math.random() * 0.5;
+        data.latRot = -data.lat / 90 * Math.PI / 2 + Math.PI / 2;
+        data.lngRot = Math.PI * data.lng / 180 + Math.PI / 3 + 0.2;
+        makeUFO(ufoObject, data.size, data.latRot, data.lngRot, data.yOff, data.rad);
+        ufoObject.updateMatrix();
+        ufoInstancedMesh.setMatrixAt(index, ufoObject.matrix);
+        data.object = ufoObject;
+        data.index = index;
+      });
+    } else {
+      console.error("UFO model structure is not as expected.");
+    }
 
     // Resize
-    window.addEventListener( 'resize', resizeWindow );
+    window.addEventListener('resize', resizeWindow);
 
     // Hover Event
     renderer.domElement.addEventListener('pointermove', onPointerMove);
- 
+
     // Animation
     let clock = new Clock();
 
     renderer.setAnimationLoop(() => {
-
       let delta = clock.getDelta();
 
       // Earth Animation
@@ -173,152 +206,56 @@ controls.enableDamping = true;
 
       // UFO Animation
       ufosData.forEach((ufoData) => {
-        let ufo = ufoData.group;
-
-        ufo.position.set(0, 0, 0);
-        ufo.rotation.set(0, 0, 0);
-        ufo.updateMatrixWorld();
-
+        const ufoObject = ufoData.object;
         ufoData.lngRot += delta * 0.03;
-        ufo.rotateOnAxis(new Vector3(0, 0, 1), ufoData.latRot); // Latitude Rotation
-        ufo.rotateOnWorldAxis(new Vector3(0, 1, 0), ufoData.lngRot); // Longtitude Rotation
-        ufo.rotateOnAxis(new Vector3(0, 1, 0), -20*ufoData.lngRot); // ufo rotation
-        ufo.translateY(ufoData.yOff);
-
-        ufoData.box.setFromObject(ufo);
+        makeUFO(ufoObject, ufoData.size, ufoData.latRot, ufoData.lngRot, ufoData.yOff, ufoData.rad);
+        ufoObject.updateMatrix();
+        ufoInstancedMesh.setMatrixAt(ufoData.index, ufoObject.matrix);
       });
+      ufoInstancedMesh.instanceMatrix.needsUpdate = true;
 
       controls.update();
       renderer.render(scene, camera);
-
     });
-} catch(err){
-  console.log(err)
-}
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
-function makeUFO(ufoMesh, scene, size, lat, lng) {
-  // ufoMesh: the Mesh of ufo
-  // Scene: Instance of THREE.scene()
-  // Size: The size / importance / progress of each project, 3 sizes: 1, 2, 3
-  // lat: Latitude North => Plus South => Minus Lookup "city name coordinates" on google
-  // lng: Langitude East => Plus West => Minus
+function makeUFO(object, size, latRot, lngRot, yOff, rad) {
+  const scale = size === 3 ? 5 : size === 2 ? 3 : 1.8;
+  object.scale.set(scale, scale, scale);
+  object.position.set(0, 0, 0);
+  object.rotation.set(0, 0, 0);
+  object.updateMatrixWorld();
 
-  // There are 3 sizes: 3, 2, 1
-  let ufo = ufoMesh.clone();
-  if (size == 3) {
-    ufo.scale.set(5, 2, 5);
-  } else if (size == 2){
-    ufo.scale.set(3, 2, 3);
-  } else if (size == 1){
-    ufo.scale.set(1.8, 2, 1.8);
-  } else console.log(`Undifined size of UFO for location {Latitude: ${lat}, longitude: ${lng}`)
-  
-  ufo.position.set(0,0,0);
-  ufo.rotation.set(0,0,0);
-  ufo.updateMatrixWorld();
-
-  ufo.traverse((o) => {
-    if (o.isMesh) {
-      o.material = new MeshPhysicalMaterial({
-      roughness: 1,
-      metalness: 1,
-      color: new Color("#dcfd7c"),
-      envMapIntensity: 1,
-      })
-      
-    // For reset color
-    if (!o.material.userData) {
-      o.material.userData = {};
-    }
-    o.material.userData.color = o.material.color.clone();
-  };
-  })
-
-
-  let group = new Group();
-  group.add(ufo);
-  scene.add(group);
-
-  // Bounding Hit Box
-  let box = new Box3().setFromObject(ufo);
-  
-  // Debug yse to see Hit Box
-  // const helper = new Box3Helper( box, 0xffff00 );
-  // scene.add(helper)
-
-  return {
-    group,
-    box,
-    rot: 0,
-    rad: Math.random() * Math.PI * 0.45 + 0.5,
-    yOff: 10.3 + Math.random() * 0.5,
-    latRot: -lat / 90 * Math.PI/2 + Math.PI/2,
-    lngRot: Math.PI * lng / 180 + Math.PI / 3 + 0.2 ,
-  };
-
-}
-
-function nr (){
-  return Math.random() * 2 - 1;
-}
-
-function resizeWindow() {
-
-  camera.aspect = innerWidth / innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( innerWidth, innerHeight );
-
+  object.rotateOnAxis(new Vector3(0, 0, 1), latRot); // Latitude Rotation
+  object.rotateOnWorldAxis(new Vector3(0, 1, 0), lngRot); // Longitude Rotation
+  object.rotateOnAxis(new Vector3(0, 1, 0), -20 * lngRot); // UFO rotation
+  object.translateY(yOff);
 }
 
 function onPointerMove(event) {
-  // console.log(ufosData)
-
-	pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-  
+
   raycaster.setFromCamera(pointer, camera);
 
-
-  // Reset Colors
-  scene.traverse( function( object ) {
-   
-    if ( object.isMesh === true &&object.name != "continent" && object.name !="ocean"){
-      object.material.color.copy( object.material.userData.color );
-    }
-  });
-
-  // const intersects = raycaster.intersectObjects(scene.children, true);
-  // for (let i = 0; i < intersects.length; i++) {
-  //   if (intersects[i].distance <= raycaster.far) {
-  //     console.log('Intersection detected within range with UFO at', intersects[i].point);
-  //     intersects[i].object.traverse((object) => {
-  //       if (object.isMesh) {
-  //         object.material.color.set(0xff0000); // Set to red or any other color
-  //       }
-  //     });
-  //     // break; // Optional: break if you only care about the first intersection
-  //   }
-  // }
-  
-  for (let ufoData of ufosData) {
-    let intersectionPoint = new Vector3(); // Create a new Vector3 to store the intersection point
-    if (raycaster.ray.intersectBox(ufoData.box, intersectionPoint) !== null) {
-      // Calculate the distance from the camera to the intersection point
-      let distance = intersectionPoint.distanceTo(camera.position);
-      if (distance <= raycaster.far) {
-        console.log('Intersection detected with UFO at', intersectionPoint, 'at distance', distance);
-        // Change color of the UFO
-        ufoData.group.traverse((object) => {
-          if (object.isMesh) {
-            object.material.color.set(0xff0000); // Set to red or any other color
-          }
-        });
-      }
+  // Adjust raycaster logic for InstancedMesh
+  const intersects = raycaster.intersectObject(ufoInstancedMesh, true);
+  if (intersects.length > 0) {
+    const instanceId = intersects[0].instanceId;
+    if (instanceId !== undefined) {
+      console.log('Intersection detected with UFO instance', instanceId);
     }
   }
+}
 
+function resizeWindow() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 
+// ... existing code ...
